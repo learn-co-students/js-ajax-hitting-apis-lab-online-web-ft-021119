02afec78eee038912f67464f5e5a983ead411733
joanrig <github email address>
2019-05-30 14:41:39 -0400

    Done.

--- js/index.js
+++ js/index.js
@@ -3,19 +3,30 @@ let github = "https://api.github.com"
 
 
 function getRepositories() {
-  const userName = document.querySelector('input#username').value
-  let req = new XMLHttpRequest()
+  const username = document.querySelector('username').value
+  const req = new XMLHttpRequest()
   req.addEventListener('load', displayRepositories)
   req.open('GET', `${github}/users/${username}/repos`)
   req.send();
 }
 
 function displayRepositories() {
-  const repos = JSON.parse(this.responseText)
-  const repoList = repos.map(r => r.name + `https//www.github.com/${username}/${full_name}`).join('')
-
+  const repos = JSON.parse(this.responseText);
+  debugger
+  console.log(repos);
+  const repoList = `<ul>${repos
+    .map(
+      r =>
+        '<li>' +
+        r.name +
+        ' - <a href="#" data-repo="' +
+        r.name +
+        '" onclick="getCommits(this)">Get Commits</a></li>'
+    )
+    .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
+
 
 function getCommits(repo) {
   const name = repo.dataset.username
